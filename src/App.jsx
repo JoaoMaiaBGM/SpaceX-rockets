43b0d835a6a89b752932486d318d82a96a53bd4f
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-09-13 15:54:16 -0300

    feat: render Header component + Global and Assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ToastContainer } from "react-toastify";
 import RoutesMain from "./Routes";
+import { GlobalStyle } from "./Global/index";
 
 function App() {
   return (
     <>
       <RoutesMain />
+      <GlobalStyle />
       <ToastContainer />
     </>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import Header from "../../Components/Header";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-      <Link to='/dashboard'>
-        <button type='button'>Ir para Dashboard</button>
-      </Link>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Link to='/dashboard'>
+          <button type='button'>Ir para Dashboard</button>
+        </Link>
+      </main>
+    </>
   );
 }
