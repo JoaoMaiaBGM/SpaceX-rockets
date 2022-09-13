eaaa455635e33abf030ba11b04fc66d1049c8669
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-09-13 14:20:58 -0300

    feat: header component structure

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <header>
+      <div className='logoContainer'>
+        <img src='' alt='' />
+        <h2>SpaceRockets</h2>
+      </div>
+      <div>
+        <nav>
+          <Link to='/home'>Home</Link>
+          <Link to='/about'>Sobre a SpaceX</Link>
+        </nav>
+        <div>
+          <input type='text' placeholder='Procure um foguete' />
+        </div>
+      </div>
+    </header>
   );
 }
