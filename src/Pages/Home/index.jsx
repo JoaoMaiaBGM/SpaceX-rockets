d47153d43170e2e21e0cde801c93d35be6013857
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-09-13 19:41:53 -0300

    feat: component header responsivity + Home page background image

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import Header from "../../Components/Header";
+import homeImg from "../../Assets/home_img.svg";
+
+import { BackgroundContainer } from "./styles";
 
 export default function Home() {
   return (
-    <>
+    <BackgroundContainer style={{ backgroundImage: `url(${homeImg})` }}>
       <Header />
       <main>
         <Link to='/dashboard'>
           <button type='button'>Ir para Dashboard</button>
         </Link>
       </main>
-    </>
+    </BackgroundContainer>
   );
 }
