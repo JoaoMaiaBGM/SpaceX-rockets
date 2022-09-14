f7c19aa270bb001dae29fdaa78f49450b3c9f239
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-09-14 17:49:17 -0300

    feat: Home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,17 +3,26 @@ import Header from "../../Components/Header";
 import homeImg from "../../Assets/home_img.svg";
 import Footer from "../../Components/Footer";
 
-import { BackgroundContainer } from "./styles";
+import { BackgroundContainer, MainContainer } from "./styles";
 
 export default function Home() {
   return (
     <BackgroundContainer style={{ backgroundImage: `url(${homeImg})` }}>
       <Header />
-      <main>
-        <Link to='/dashboard'>
-          <button type='button'>Ir para Dashboard</button>
-        </Link>
-      </main>
+      <MainContainer>
+        <div>
+          <h1>SpaceX rockets</h1>
+          <p>
+            Venha descobrir informações e curiosidades sobre os foguetes dessa
+            empresa que mais cresce no mundo. Pesquise por um nome de foguete
+            específico ou clique no botão abaixo para ser redirecionado(a) para
+            a nossa dashboard.
+          </p>
+          <Link to='/dashboard'>
+            <button type='button'>Conheça os foguetes</button>
+          </Link>
+        </div>
+      </MainContainer>
       <Footer />
     </BackgroundContainer>
   );
